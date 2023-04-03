39b0d935ee2abcdfd7a3577a00b5d30720993936
Daniel Kim <>
2023-04-03 11:07:31 -0700

    let fruit return to the original location

--- src/main/java/com/ngin/Match3EventHandler.java
+++ src/main/java/com/ngin/Match3EventHandler.java
@@ -8,6 +8,8 @@ import commander.Command.TouchMotion;
 public class Match3EventHandler extends EventHandler {
     Nx nx;
     int id;
+    int x;
+    int y;
     int[][] table;
     String[] fruits;
     Match3EventHandler(Nx n, int[][] t, String[] f) {
@@ -19,13 +21,15 @@ public class Match3EventHandler extends EventHandler {
     @Override
     public void onTap(TapInfo info) throws IOException, InterruptedException {
         if (info.event == TouchMotion.DOWN_VALUE) {
-            int xn = (int)Math.floor(info.x);
-            int yn = (int)Math.floor(info.y);
-            id = yn*10+xn;
+            x = (int)Math.floor(info.x);
+            y = (int)Math.floor(info.y);
+            id = y*10+x;
 
-            System.out.println(fruits[table[yn][xn]]);
+            System.out.println(fruits[table[y][x]]);
         } else if (info.event == TouchMotion.MOVE_VALUE) { 
             nx.sendTransform(new Transform().setTranslating(info.x, info.y), id, 0);
+        } else {
+            nx.sendTransform(new Transform().setTranslating(x + 0.5f, y + 0.5f), id, 0);            
         }
     }
     
