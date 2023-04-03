89cb2c5ced20cd0f2a3fe3228bd2909c4c6979c3
Daniel Kim <>
2023-04-03 10:12:56 -0700

    x

--- src/main/java/com/ngin/Match3EventHandler.java
+++ src/main/java/com/ngin/Match3EventHandler.java
@@ -3,10 +3,11 @@ package com.ngin;
 import java.io.IOException;
 import java.util.List;
 
+import commander.Command.TouchMotion;
+
 public class Match3EventHandler extends EventHandler {
     Nx nx;
-    float x_old;
-    float y_old;
+    float x, y;
     int[][] table;
     String[] fruits;
     Match3EventHandler(Nx n, int[][] t, String[] f) {
@@ -17,49 +18,14 @@ public class Match3EventHandler extends EventHandler {
 
     @Override
     public void onTap(TapInfo info) throws IOException {
-        //unexpected(info.toString());
-        //System.out.println(info.toString());
+        if (info.event == TouchMotion.DOWN_VALUE) {
+            x = info.x;
+            y = info.y;
+            int xn = (int)Math.floor(info.x);
+            int yn = (int)Math.floor(info.y);
 
-        if (info.event == 1) {
-            x_old = info.x;
-            y_old = info.y;
-            int x = (int)Math.floor(info.x);
-            int y = (int)Math.floor(info.y);
-            int oid = y*10 + x;
-            System.out.println(fruits[table[y][x]]);
-            //nx.remove(y*10 + x);
-            //nx.linearTo(oid, x, y+1, 5);
-
-        } else if (info.event == 4) {
-            float x_diff = info.x - x_old;
-            float y_diff = info.y - y_old;
-
-            float x_abs2 = x_diff>0?x_diff:-x_diff;
-
-            float x_abs = Math.abs(x_diff);
-            float y_abs = Math.abs(y_diff);
-
-            if (x_abs > y_abs) {
-                if (x_diff > 0) {
-                    System.out.println("RIGHT");
-                } else {
-                    System.out.println("LEFT");
-                }
-            } else {
-                if (y_diff > 0) {
-                    System.out.println("DOWN");
-                } else {
-                    System.out.println("UP");
-                }
-            }
-
-
+            System.out.println(fruits[table[yn][xn]]);
         }
-
-        //int x = (int)Math.floor(info.x);
-        //int y = (int)Math.floor(info.y);
-        //nx.transform();
-        //nx.remove(y*10 + x);
     }
     
 }
