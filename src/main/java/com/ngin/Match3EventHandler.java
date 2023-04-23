9b997abf85c8a95e662ae9338c1669d208bebf9a
Daniel Kim <>
2023-04-22 22:23:41 -0700

    Vec2

--- src/main/java/com/ngin/Match3EventHandler.java
+++ src/main/java/com/ngin/Match3EventHandler.java
@@ -8,10 +8,26 @@ import commander.Command.TouchMotion;
 public class Match3EventHandler extends EventHandler {
     Nx nx;
     int id;
-    int x;
-    int y;
+    Vec2 ori;
     int[][] table;
     String[] fruits;
+
+    class Vec2 {
+        float x;
+        float y;
+        Vec2(float f0, float f1) {
+            x = f0;
+            y = f1;
+        }
+
+        int xFloored() {
+            return (int)Math.floor(x);
+        }
+        int yFloored() {
+            return (int)Math.floor(y);
+        }        
+    }
+
     Match3EventHandler(Nx n, int[][] t, String[] f) {
         nx = n;
         table = t;
@@ -21,15 +37,14 @@ public class Match3EventHandler extends EventHandler {
     @Override
     public void onTap(TapInfo info) throws IOException, InterruptedException {
         if (info.event == TouchMotion.DOWN_VALUE) {
-            x = (int)Math.floor(info.x);
-            y = (int)Math.floor(info.y);
-            id = y*10+x;
+            ori = new Vec2(info.x, info.y);
+            id = ori.yFloored()*10+ori.xFloored();
 
-            System.out.println(fruits[table[y][x]]);
+            //System.out.println(fruits[table[y][x]]);
         } else if (info.event == TouchMotion.MOVE_VALUE) { 
             nx.sendTransform(new Transform().setTranslating(info.x, info.y), id, 0);
         } else {
-            nx.sendTransform(new Transform().setTranslating(x + 0.5f, y + 0.5f), id, 0);            
+            nx.sendTransform(new Transform().setTranslating(ori.xFloored() + 0.5f, ori.yFloored() + 0.5f), id, 0);            
         }
     }
     
