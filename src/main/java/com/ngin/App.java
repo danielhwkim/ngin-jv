d95ee07e54b517c03cdce6e3fae3034476b61461
Daniel Kim <>
2023-05-27 08:30:45 -0700

    1.0.16

--- src/main/java/com/ngin/App.java
+++ src/main/java/com/ngin/App.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 
 public class App 
 {
-    public static void main2( String[] args ) throws IOException, InterruptedException
+    public static void main( String[] args ) throws IOException, InterruptedException
     {
         new M3().run();
     }
