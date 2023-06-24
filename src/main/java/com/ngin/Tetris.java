1bbfe5ac484376c89a07ced9b2ac5ae70ffccf6c
Daniel Kim <>
2023-06-24 16:53:26 -0700

    move one block to bottom at the same time

---
+++ src/main/java/com/ngin/Tetris.java
@@ -0,0 +1,57 @@
+package com.ngin;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import com.google.common.collect.HashMultimap;
+import com.ngin.Nx.Vector2;
+import com.ngin.Nx.Visible;
+
+public class Tetris extends EventHandler {
+    Nx nx;
+
+    Tetris() throws IOException {
+        nx = new Nx();
+    }
+
+    @Override
+    public void onTap(TapInfo info) throws IOException, InterruptedException {
+        // TODO Auto-generated method stub
+        super.onTap(info);
+    }
+
+    public void run() throws IOException, InterruptedException {
+        nx.new Stage(10, 20).enableDebug(true).sendWithAck();
+        HashMap<Integer, Visible> m = new HashMap<>();
+        
+        m.put(100, nx.new Visible(0.5f, 0.5f).addClip("Background/Yellow.png", 64, 64));
+        m.put(101, nx.new Visible(0.5f, 1.5f).addClip("Background/Yellow.png", 64, 64));
+        m.put(102, nx.new Visible(0.5f, 2.5f).addClip("Background/Yellow.png", 64, 64));
+        m.put(103, nx.new Visible(1.5f, 2.5f).addClip("Background/Yellow.png", 64, 64));
+
+        for (int i : m.keySet()) {
+            m.get(i).send(i);
+        }
+
+        for (int i : m.keySet()) {
+            Visible v = m.get(i);
+            Vector2 v2 = v.getPos().add(nx.new Vector2(0, 10));
+
+            v.setPos(v2);
+            nx.new Transform().translate(v2).send(i, 2.5f);
+        }        
+
+        nx.runEventLoop(this);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        try {
+            new T().run();
+        } catch (Exception e) {
+            System.out.println("Exception - " + e.toString());
+        }
+    }
+    
+}
